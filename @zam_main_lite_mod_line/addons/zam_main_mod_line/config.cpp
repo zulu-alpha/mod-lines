23823aee6959caa68b7c1320046fa6a2fa084d2d
Adam Piskorski <adam@piskorski.me>
2019-03-03 18:21:00 +0200

    feat: Add main lite mod line

---
+++ @zam_main_lite_mod_line/addons/zam_main_mod_line/config.cpp
@@ -0,0 +1,14 @@
+class CfgPatches {
+	class zam_main_lite_mod_line
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {};
+		version = "0.1.0";
+		versionStr = "0.1.0";
+		versionDesc = "zam_main_lite_mod_line";
+		versionAr[] = {0,1,0};
+		author[]= {"Zulu-Alpha"};
+		authorUrl = "http://zulu-alpha.co.za/";
+	};
+};
---
+++ @zam_main_lite_mod_line/mod.cpp
@@ -0,0 +1,17 @@
+name = "Zulu-Alpha - Main Lite Mod Line";
+picture = "Data\picture.paa";
+logoSmall = "Data\logo_small.paa";
+logo = "Data\logo.paa";
+logoOver = "Data\logo_over.paa";
+action = "https://github.com/zulu-alpha/mod-lines";
+tooltipOwned = "Zulu-Alpha - Main Lite Mod Line";
+dlcColor[] = 
+{
+	0.26,
+	0.17,
+	0.12,
+	1
+};
+overview = "The purpose of this mod is to have all the mods of the Zulu-Alpha - Main Lite Mod Line as dependencies in the Steam Workshop";
+hideName = 0;
+hidePicture = 0;
