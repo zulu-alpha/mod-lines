ef60f6e72576e91b63fb888c457b1892a870a0a9
Adam Piskorski <adam@piskorski.me>
2023-08-11 21:57:53 +0200

    feat: Add optional mod line mod

---
+++ @zam_optional_mod_line/mod.cpp
@@ -0,0 +1,16 @@
+name = "Zulu-Alpha - Optional Mod Line";
+picture = "Data\picture.paa";
+logoSmall = "Data\logo_small.paa";
+logo = "Data\logo.paa";
+logoOver = "Data\logo_over.paa";
+action = "https://github.com/zulu-alpha/mod-lines";
+tooltipOwned = "Zulu-Alpha - Optional Mod Line";
+dlcColor[] =
+	{
+		0.26,
+		0.17,
+		0.12,
+		1};
+overview = "The purpose of this mod is to have all the mods of the Zulu-Alpha - Optional Mod Line as dependencies in the Steam Workshop";
+hideName = 0;
+hidePicture = 0;
